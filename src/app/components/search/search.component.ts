68cc8143cf45ba617a1ae8ac1a855552d7ca2291
Francisco Lopez <franlopbri@gmail.com>
2020-10-06 16:33:34 -0500

    se implementa la busqueda en el component y el servicio

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -6,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  artists: any[] = []
 
-  ngOnInit(): void {
+  constructor(private _spotifyService: SpotifyService) { }
+
+  buscar(termino: string){
+    console.log(termino);
+    this._spotifyService.getArtist(termino)
+        .subscribe( (data: any)  => {
+          console.log(data.artists.items);
+          this.artists = data.artists.items;
+        });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -16,6 +16,14 @@ export class SpotifyService {
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQAHxfRt4M_hKoohFn3Z_WhtAvfbm4_kbTTNuPDBCr7CR0LD3IukXj1aEy8YiHxGpdS1BVGHEpP_Uoe7cTo'
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+
+  }
+
+  getArtist( termino:string ){
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQAHxfRt4M_hKoohFn3Z_WhtAvfbm4_kbTTNuPDBCr7CR0LD3IukXj1aEy8YiHxGpdS1BVGHEpP_Uoe7cTo'
+    });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=10`, { headers });
   }
 }
