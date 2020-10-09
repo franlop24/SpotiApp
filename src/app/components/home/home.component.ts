16c9e97544fe08f2faa3d5025fb6e7f3555cf711
Francisco Lopez <franlopbri@gmail.com>
2020-10-09 13:17:12 -0500

    manejando errores

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,16 +11,22 @@ export class HomeComponent implements OnInit {
 
   nuevasCanciones: any[] = [];
   loading: boolean;
+  error: boolean;
+  mensajeError: string;
 
   constructor(private _spotifyService: SpotifyService) {
 
     this.loading = true;
-
+    this.error = false;
     this._spotifyService.getNewReleases()
         .subscribe( (data: any) => {
           this.nuevasCanciones = data;
           this.loading = false;
-        } );
+        }, (errorService) => {
+          this.error = true
+          console.log(errorService);
+          this.mensajeError = errorService.error.error.message;
+        });
   }
 
   ngOnInit(): void {
